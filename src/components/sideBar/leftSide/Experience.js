84683055f4ef0aa0c36dd52ddae29b4cfd984fba
sabuj hasan sarker <sabujhasansarker@gmail.com>
2020-06-07 01:26:55 +0600

    education

--- src/components/sideBar/leftSide/Experience.js
+++ src/components/sideBar/leftSide/Experience.js
@@ -1,10 +1,59 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
+
+import style from "./Style.module.css";
 
 const Experience = () => {
+  const [addToggle, setAddToggle] = useState([{ id: 1 }]);
+  const { from, fromGroup } = style;
   return (
-    <div>
-      <h1>Experience</h1>
-    </div>
+    <Fragment>
+      {addToggle.length > 0 &&
+        addToggle.map((e) => (
+          <form key={e.id} className={fromGroup}>
+            <div className="formGroup">
+              <label>School Name</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="School name"
+                name="school"
+              />
+            </div>
+            <div className="formGroup">
+              <label>Group</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Group"
+                name="group"
+              />
+            </div>
+            <div className="formGroup">
+              <label>Passing Year</label>
+              <input
+                type="date"
+                className="form-control"
+                placeholder="Passing Year"
+                name="passing year"
+              />
+            </div>
+            <div className="formGroup">
+              <label>Result</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Result"
+                name="result"
+              />
+            </div>
+          </form>
+        ))}{" "}
+      <button
+        onClick={(e) => setAddToggle([...addToggle, { id: Math.random() }])}
+      >
+        add
+      </button>
+    </Fragment>
   );
 };
 
