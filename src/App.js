7aeb1d177d4a08e10c1912707c4b3d65327f3a4e
sabuj hasan sarker <sabujhasansarker@gmail.com>
2020-06-10 18:51:13 +0600

    responsive

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 // Components
 import Print from "./components/layout/Print";
 import LeftIndex from "./components/sideBar/leftSide/LeftIndex";
@@ -7,18 +7,53 @@ import DataState from "./context/DataState";
 import Contect from "./components/contents/Contect";
 
 const App = () => {
+  const [toggle, setToggle] = useState("center");
   return (
     <DataState>
       <div className="gridContainer">
-        <div className="left">
+        {toggle !== "left" && (
+          <div className="leftScreen" onClick={() => setToggle("left")}>
+            <img
+              src="https://img.icons8.com/plasticine/100/000000/chevron-right.png"
+              alt=""
+              className="arro_img"
+            />
+          </div>
+        )}
+        <div className={`left ${toggle === "left" && "leftOpen"}`}>
           <LeftIndex />
+          <div className="leftScreen" onClick={() => setToggle()}>
+            <img
+              src="https://img.icons8.com/plasticine/100/000000/chevron-right.png"
+              alt=""
+              className="arro_img"
+            />
+          </div>
         </div>
-        <div className="center">
+        <div className={`center `}>
           <Contect />
           <Print />
         </div>
-        <div className="right">
+        {toggle !== "right" && (
+          <div className="rightScreen" onClick={() => setToggle("right")}>
+            <img
+              src="https://img.icons8.com/plasticine/100/000000/chevron-left.png"
+              alt=""
+              className="arro_img"
+            />
+          </div>
+        )}
+        <div className={`right ${toggle === "right" && "rightOpen"}`}>
           <RightSide />
+          {toggle === "right" && (
+            <div className="rightScreen" onClick={() => setToggle()}>
+              <img
+                src="https://img.icons8.com/plasticine/100/000000/chevron-left.png"
+                alt=""
+                className="arro_img"
+              />
+            </div>
+          )}
         </div>
       </div>
     </DataState>
